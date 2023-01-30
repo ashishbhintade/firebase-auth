6bc7cb3e4e4fd8e381aa2ef9421e9284b5f6e0fe
Ashish Bhintade <68814285+ashishbhintade@users.noreply.github.com>
2023-01-30 18:26:30 +0530

    Create new user

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,17 +1,26 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
   onAuthStateChanged,
 } from "firebase/auth";
+import { auth } from "../firebase";
 
 const UserContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  return <UserContext.Provider>{children}</UserContext.Provider>;
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  return (
+    <UserContext.Provider value={{ createUser }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 export const UserAuth = () => {
-  return UserContext(UserContext);
+  return useContext(UserContext);
 };
